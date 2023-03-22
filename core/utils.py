68ed6753fb7ee7244f111fb7b2e375972ac2d682
LuisHenrique01 <luizbueno1201@gmail.com>
2023-03-22 15:40:00 -0300

    Feat: add qual_tipo_chave_pix

--- core/utils.py
+++ core/utils.py
@@ -1,4 +1,5 @@
 import os
+import re
 from secrets import token_hex
 
 
@@ -12,7 +13,7 @@ def gerar_codigo() -> str:
     return token_hex(8).upper()
 
 
-def valida_cpf(cpf: str) -> bool:
+def cpf_valido(cpf: str) -> bool:
     """
     Valida um CPF, retornando True se for válido ou False caso contrário.
 
@@ -45,3 +46,42 @@ def valida_cpf(cpf: str) -> bool:
         return True
     else:
         return False
+
+
+def formato_cpf_valido(cpf: str) -> bool:
+    if re.match(r'\d{3}\.\d{3}\.\d{3}-\d{2}', cpf):
+        return True
+    return False
+
+
+def formato_telefone_valido(telefone: str) -> bool:
+    if re.match(r'^\([1-9]{2}\)(?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$', telefone):
+        return True
+    return False
+
+
+def formato_email_valido(email: str) -> bool:
+    if re.match(r'(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)', email):
+        return True
+    return False
+
+
+def qual_tipo_chave_pix(chave_pix: str) -> str:
+
+    chave_pix = chave_pix.replace(" ", "")
+
+    if formato_email_valido(chave_pix):
+        return "e-mail"
+
+    if cpf_valido(chave_pix):
+        return "CPF"
+
+    if formato_telefone_valido(chave_pix) or \
+       (chave_pix.isnumeric() and (len(chave_pix) == 10 or len(chave_pix) == 11)):
+        return "telefone"
+
+    if len(chave_pix) == 14 and chave_pix.isnumeric():
+        return "CNPJ"
+
+    if len(chave_pix) == 32 and chave_pix.isalnum():
+        return "aleatorio"
